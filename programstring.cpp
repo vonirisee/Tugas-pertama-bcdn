f9e75676c81e6e6fd3b68b3465d6b90b4756d1ce
vonirise <masumifelix7@gmail.com>
2025-07-06 00:23:28 +0800

    program string

---
+++ programstring.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string pesanan;
+
+    cout << "Selamat datang di tempat mangan <3 " << endl;
+cout<<"Masukan pesenan (jangan pake sepasi): ";
+    cin>>pesanan;
+         cout << "baik!," << pesanan << " segera dibuat! " <<endl;
+}
