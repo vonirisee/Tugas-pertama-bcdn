95f044638275419d895b76eb340a1f04c54fcaa4
vonirise <masumifelix7@gmail.com>
2025-07-10 07:50:57 +0800

    program float

---
+++ programfloat.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+    int a, b, jumlah;
+    
+    cout<<"angka pertama: ";
+    cin>>a;
+
+    cout<<"angka kedua: "; 
+         cin>>b;
+             jumlah = a + b; 
+    cout<<"Hasil penjumlahan: " << jumlah << endl; 
+}
